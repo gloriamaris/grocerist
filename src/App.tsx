d67b10e504896da47fe503845e156163ca532c4f
Monique Dingding <monique.dingding@lamudi.com>
2019-07-05 10:42:29 +0800

    convert states to Hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,67 +1,48 @@
-import React, { Component } from 'react' 
+import React, { useState } from 'react'
 import NewItemForm from './components/NewItemForm/NewItemForm'
 import ItemsList from './components/ItemsList/ItemsList'
 import { Item } from './models/item'
 
-interface State {
-  newItem: Item,
-  items: Item[]
-}
 
-class App extends Component<{}, State> {
-  state = {
-    newItem: {
-      id: 1,
-      name: ''
-    },
-    items: []
-  }
+const App = () => {
+  const [newItem, setNewItem] = useState<Item>({ id: 0, name: '' })
+  const [items, setItems] = useState<Item[]>([])
 
-  private addItem = (event: React.FormEvent<HTMLFormElement>) => {
+  const addItem = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault()
-
-    this.setState(prevState => ({
-      newItem: {
-        id: prevState.newItem.id + 1,
-        name: prevState.newItem.name
-      },
-      items: [ ...prevState.items, prevState.newItem ]
-    }))
+    const item = { id: newItem.id + 1, name: newItem.name }
+    
+    setNewItem({ ...item })
+    setItems([item, ...items])
   }
 
-  private handleItemChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    this.setState({
-      newItem: {
-        ...this.state.newItem,
-        name: event.target.value
-      }
+  const handleItemChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setNewItem({
+      ...newItem,
+      name: event.target.value
     })
   }
 
-  private deleteItem = (itemToDelete: Item) => {
-    this.setState(prevState => ({
-      items: [ ...prevState.items.filter(item => item.id !== itemToDelete.id)]
-    }))
+  const deleteItem = (itemToDelete: Item) => {
+    setItems([...items.filter(item => item.id !== itemToDelete.id)])
   }
 
-  render () {
-    const { newItem, items } = this.state
-    
-    return (
-      <div>
-        <h3>Grocerist</h3>
-        <NewItemForm
-          item={newItem}
-          onAdd={this.addItem}
-          onChange={this.handleItemChange}
-        />
-        <ItemsList 
-          items={items}
-          onDelete={this.deleteItem}
-        />
-      </div>
-    )
-  }
+  console.log({ items })
+  return (
+    <div>
+      <h3>Grocerist</h3>
+      <NewItemForm
+        item={newItem}
+        onAdd={addItem}
+        onChange={handleItemChange}
+      />
+      <ItemsList
+        items={items}
+        onDelete={deleteItem}
+      />
+    </div>
+  )
+
 }
 
 export default App
